c492fe40106350fa507f24d9f00227b3b2c8ef45
Bi Xiaosong <bixiaosong@gmail.com>
2023-11-04 12:02:04 +0100

    initial

---
+++ .history/items_20231104114713.js
@@ -0,0 +1,167 @@
+;[
+  {
+    id: 1,
+    category: 'ENTREES',
+    items: [
+      {
+        id: 101,
+        name: 'Salade verte',
+        contents: '',
+        small_price: '3,00€',
+        large_price: '5,00€',
+      },
+      {
+        id: 102,
+        name: 'Salade mixte',
+        contents: 'salade, tomates, jambon, olives, champignons',
+        small_price: '6,00€',
+        large_price: '8,00€',
+      },
+      {
+        id: 103,
+        name: 'Salade gésiers lardons',
+        contents: '',
+        small_price: '6,50€',
+        large_price: '9,00€',
+      },
+      {
+        id: 104,
+        name: 'Salade chèvre chaud',
+        contents: 'salade, tomates, chèvre chaud, jambon Serrano',
+        small_price: '6,50€',
+        large_price: '9,00€',
+      },
+      {
+        id: 105,
+        name: "Salade d'anchois",
+        contents: 'salade, tomates, anchois, olives',
+        small_price: '6,50€',
+        large_price: '9,00€',
+      },
+      {
+        id: 106,
+        name: 'Assiette jambon Serrano',
+        contents: '',
+        small_price: '6,50€',
+        large_price: '9,00€',
+      },
+      {
+        id: 107,
+        name: 'Salade pétoncles persillés',
+        contents: '',
+        small_price: '7,00€',
+        large_price: '10,00€',
+      },
+      {
+        id: 108,
+        name: 'Salade au cantal',
+        contents: 'salade, tomates, cantal, jambon serrano',
+        small_price: '6,50€',
+        large_price: '9,00€',
+      },
+      {
+        id: 109,
+        name: 'Salade tomate mozzarella di bufala',
+        contents:
+          "Tomates, mozzarella di bufala, basilic, pesto, huile d'olive",
+        small_price: '7,00€',
+        large_price: '10,00€',
+      },
+      {
+        id: 110,
+        name: 'Salade marine',
+        contents:
+          'salade, tomates, pétoncles, saumon fumé, crevettes, persillade',
+        small_price: '8,00€',
+        large_price: '11,50€',
+      },
+      {
+        id: 111,
+        name: 'Salade gourmande',
+        contents: 'Salade, tomates, gésiers, magret, bloc de foie gras, olives',
+        small_price: '8,00€',
+        large_price: '11,50€',
+      },
+      {
+        id: 112,
+        name: 'Salade burrata',
+        contents: 'burrata, tomates, basilic, jambon serrano, pesto, roquette',
+        small_price: '8,00€',
+        large_price: '11,50€',
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: 'PATES',
+    items: [
+      {
+        id: 201,
+        name: 'Tagliatelles carbonara',
+        contents: 'champignons, lardons, oignons, crème, oeuf',
+        price: '10,50€',
+      },
+      {
+        id: 202,
+        name: 'Tagliatelles au roquefort',
+        contents: '',
+        price: '10,50€',
+      },
+      {
+        id: 203,
+        name: 'Tagliatelles bolognaise',
+        contents: '',
+        price: '10,50€',
+      },
+      {
+        id: 204,
+        name: 'Tagliatelles au pesto',
+        contents: '',
+        price: '11,00€',
+      },
+      {
+        id: 205,
+        name: 'Tagliatelles aux pétoncles persillés',
+        contents: '',
+        price: '12,00€',
+      },
+      {
+        id: 206,
+        name: 'Tagliatelles au saumon fumé',
+        contents: '',
+        price: '12,00€',
+      },
+      {
+        id: 207,
+        name: 'Tagliatelles au pesto et burrata',
+        contents: '',
+        price: '15,00€',
+      },
+    ],
+  },
+][
+  {
+    id: 3,
+    category: 'VIANDES',
+    items: [
+      {
+        id: 301,
+        name: 'Poulet au curry',
+        contents: '',
+        price: '10,50€',
+      },
+      {
+        id: 302,
+        name: 'Cuisse de canard confite',
+        contents: '',
+        price: '12,50€',
+      },
+      {
+        id: 303,
+        name: 'Magret de canard',
+        contents: '',
+        price: '17,50€',
+      },
+    ],
+  }
+]
